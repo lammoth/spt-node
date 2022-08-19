2dadec95cc106b11a4c9ca8298933c5d3a86f2e3
Francisco Hidalgo <chelestra2003@yahoo.es>
2022-08-19 09:53:40 +0200

    Removing unused info

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,9 +2,6 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CartoService } from 'src/services/carto/carto.service';
 import { CartoAuthGuard } from 'src/modules/auth/carto-auth.guard';
-//import { AuthGuard } from '@nestjs/passport';
-
-// import { UserPasswordData } from './users.model';
 
 @Controller({
   path: 'users',
@@ -17,9 +14,8 @@ export class UsersController {
   ) {}
 
   @UseGuards(CartoAuthGuard)
-  //@UseGuards(AuthGuard('carto'))
-  @Get('cartotest')
-  async test_carto(): Promise<any> {
-    //return await this.cartoService.jobs();
+  @Get()
+  async info(): Promise<any> {
+    return await this.cartoService.jobs();
   }
 }
